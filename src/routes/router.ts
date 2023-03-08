2be3b0ec97918129c42376c52efca6dbdebf491f
blackanthony14 <anthony.duffian@gmail.com>
2023-03-08 16:59:47 -0600

    Last Changes

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,12 +1,8 @@
 import { Application } from "express";
-import imagesController from "../controllers/images.controller";
-import multer from "multer";
-const upload = multer();
+import images from "./images";
 export default function router(app: Application): void {
   /**
    * Every source are specifed here
    */
-  app.get(`/:id`, (req, res) => imagesController.getImage(req, res))
-  app.post("/upload", upload.single("image"), (req, res) =>
-    imagesController.uploadImage(req, res))
+  app.use("/",images);
 }
