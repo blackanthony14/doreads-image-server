b20eb23d0df5b07dfcc50db60e73ca04d81fc904
wmartzh <wmartzh@gmail.com>
2023-03-11 19:29:34 -0600

    add cors

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,13 @@
 import { Application, json } from "express";
 import morgan from "morgan";
+import cors from "cors";
+const whitelist = process.env.ALLOWED_ORIGINS?.split(",");
 
+const corsOptions: cors.CorsOptions = {
+  origin: whitelist || false,
+};
 export function configApp(app: Application) {
+  app.use(cors(corsOptions));
   app.use(json());
   app.use(morgan("tiny"));
 }
